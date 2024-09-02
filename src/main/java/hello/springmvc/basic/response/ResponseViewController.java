62805dfc0edf77d46395e9992ebbb407281604a0
jihae <dkswlgo6615@naver.com>
2024-09-02 15:03:49 +0900

    0902 HTTP 응답, 메시지 컨버터

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.response;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
